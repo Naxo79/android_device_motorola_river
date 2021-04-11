7d4f52a60e0d0a7f27f9d10d44566f6a3229e78b
HsyHusker <misterhusker19@gmail.com>
2021-04-11 12:08:00 +0800

    river: disable qs cutout space - Disabled to test

--- device.mk
+++ device.mk
@@ -23,7 +23,8 @@ $(call inherit-product, vendor/motorola/river/river-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-dot
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
